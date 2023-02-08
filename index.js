3442815c0c3ea9fd541f2e42ec2efcc8552739d3
bk408 <bhavyakhatri1912@gmail.com>
2023-02-08 16:42:45 +0530

    stripe added

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+
+
+const stripe = require("./routes/stripe");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/stripe", stripe);
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to EKart");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`server running on port ${port}`));
+
+
+
+
