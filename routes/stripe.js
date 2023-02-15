57ed65a15903389f48b48e7851d0d9528a7bf740
bk408 <bhavyakhatri1912@gmail.com>
2023-02-15 16:16:47 +0530

    add shipping address, mobile number and card option

--- routes/stripe.js
+++ routes/stripe.js
@@ -29,6 +29,54 @@ router.post('/create-checkout-session', async (req, res) => {
 
 
     const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      
+  shipping_options: [
+    {
+      shipping_rate_data: {
+        type: 'fixed_amount',
+        fixed_amount: {
+          amount: 0, 
+          currency: 'usd',
+        },
+        display_name: 'Free shipping',
+        delivery_estimate: {
+          minimum: {
+            unit: 'business_day', 
+            value: 5,
+          },
+          maximum: {
+            unit: 'business_day',
+             value: 7,
+            },
+        },
+      },
+    },
+    {
+      shipping_rate_data: {
+        type: "fixed_amount",
+        fixed_amount: {
+          amount: 1500,
+          currency: "usd",
+        },
+        display_name: "Next day air",
+        // Delivers in exactly 1 business day
+        delivery_estimate: {
+          minimum: {
+            unit: "business_day",
+            value: 1,
+          },
+          maximum: {
+            unit: "business_day",
+            value: 1,
+          },
+        },
+      },
+    },
+  ],
+   phone_number_collection: {
+    enabled: true,
+  },
       line_items,
       mode: 'payment',
       success_url: `${process.env.CLIENT_URL}/checkout-success`,
